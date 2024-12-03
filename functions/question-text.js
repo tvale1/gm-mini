9e6f1ab74db55067fd7c0ae8acbacc9546ad37e5
tvale1 <114479470+tvale1@users.noreply.github.com>
2024-12-04 02:41:22 +1100

    Add files via upload

---
+++ functions/question-text.js
@@ -0,0 +1,200 @@
+export function questionText(questionObj, questionString){
+    
+    // add paragraph text
+    if (Object.keys(questionObj)[0] == "paragraph-text") {
+        questionString += "<p>" + questionObj['paragraph-text'] + "</p>";
+    }
+
+    //add inline italics
+    if (Object.keys(questionObj)[0] == "italics-inline") {
+        questionString = questionString.slice(0,-4);
+        questionString += "<i>" + questionObj['italics-inline'] + "</i></p>";
+    };
+
+    //add inline text
+    if (Object.keys(questionObj)[0] == "text-inline") {
+        questionString = questionString.slice(0,-4);
+        questionString += questionObj['text-inline'] + "</p>";
+    };
+    
+    //add equation
+    if (Object.keys(questionObj)[0] == "equation") {
+        questionString += "$$" + questionObj['equation'] + "$$";
+        
+    };
+
+    //add inline equation
+    if (Object.keys(questionObj)[0] == "equation-inline") {
+        questionString = questionString.slice(0,-4);
+        questionString += "#" + questionObj['equation-inline'] + "#</p>";
+    };
+
+    //add inline equation
+    if (Object.keys(questionObj)[0] == "image") {
+        // questionString = questionString.slice(0,-4);
+        questionString += "<div><img width = 80% src = '../images/"+ questionObj['image'] +"'></div>";
+    };
+     //add inline equation
+     if (Object.keys(questionObj)[0] == "superscript-inline") {
+        questionString = questionString.slice(0,-4);
+        questionString += "<sup>" + questionObj['superscript-inline'] + "</sup></p>";
+    };
+    
+    
+    return questionString;
+}
+
+export function mcOptionsText(optionsArray, questionString){
+    const mcLabels = ["A. ", "B. ", "C. ", "D. "];
+    questionString +="<div class='options-container'>"
+    for (let n=0; n<optionsArray.length; n++){
+        questionString += "<p class='mc-options'><strong style='font-family: Arial;'>"+ mcLabels[n] +"</strong></p>"
+
+        for(let p=0; p<optionsArray[n].length; p++){
+            if (Object.keys(optionsArray[n][p]) == "text-inline"){
+                questionString = questionString.slice(0,-4);
+                questionString += optionsArray[n][p]['text-inline'] + "</p>";
+            }
+        }
+    }
+
+    questionString +="</div>"
+    return questionString;
+}
+
+export function optionsClick(e){
+    
+    const endgame = document.querySelector(".end-game");
+    let correctAnswer = e.currentTarget.correctAnswer;
+    let score = e.currentTarget.playerData['score']
+    let streak = e.currentTarget.playerData['streak']
+    let lastFinishedDate = e.currentTarget.playerData['last-game-finished-date'];
+    let total = e.currentTarget.playerData['total'];
+    let time = timeUntilEndOfDay();
+    let daysBetween = daysBetweenTwoDates(lastFinishedDate, todaysDate());
+
+    if (daysBetween >=2){
+        streak = 0;
+    }
+
+    e.currentTarget.playerData['last-game-finished-date'] = todaysDate();
+    
+    //update total
+    total++
+    e.currentTarget.playerData['total'] = total;
+    
+    
+    
+    //If correct answer is selected
+    if (e.target.textContent.slice(0,1) == correctAnswer) {
+        //Update Score
+        score++;
+        e.currentTarget.playerData['score'] = score; 
+
+        //Update Streak
+        streak++;
+        e.currentTarget.playerData['streak'] = streak;
+        
+        //Update Local Storage
+        let strPlayerData = JSON.stringify(e.currentTarget.playerData);
+        localStorage.setItem("playerData", strPlayerData);
+
+        //Display win screen
+        e.target.style.backgroundColor = "lightskyblue";
+        displayEndGame(endgame, score, time, "rgba(0, 92, 54, 0.9)", "Correct", correctAnswer, streak, total);
+        
+    }
+   
+    //If wrong answer is selected
+    else {
+        //Update Score - no need, it remains unchanged
+    
+        //Update Streak - reset to 0
+        streak = 0;
+        e.currentTarget.playerData['streak'] = streak;
+        
+        //Update Local Storage
+        let strPlayerData = JSON.stringify(e.currentTarget.playerData);
+        localStorage.setItem("playerData", strPlayerData);
+        
+        e.target.style.backgroundColor = "rgb(241, 92, 92)";
+        displayEndGame(endgame, score, time, "rgba(255, 76, 76, 0.9)", "Incorrect", correctAnswer, streak, total);
+        
+    }
+    
+}
+
+export function displayEndGame(endgame, score, time, colour, correct, correctAnswer, streak, total){
+    endgame.style.display = "flex";
+    endgame.style.visibility = "visible";
+    endgame.style.opacity = "1";
+
+    endgame.style.backgroundColor = colour;
+    endgame.innerHTML = `
+<p style="font-size: 2em">${correct}</p>
+        <p style="font-size: 1.5em">${(correct == "Correct")? "Keep it up tomorrow" : (correct == "Incorrect") ? "Try again tomorrow": ""}</p>
+        ${correct == "Correct" ? `<p style='font-size: 1.5em'>Your streak is ${streak}</p>` : (correct == "Incorrect") ? `<p style='font-size: 1.5em'>The correct answer is ${correctAnswer}</p>` : (streak>0) ? `<p style='font-size: 1.5em'>Your streak is ${streak}</p>` :""}
+        <p style="font-size: 1.5em">Your percentage is ${Math.round(score/total*100)}%</p>
+        <p style="font-size: 1.5em">Time until next puzzle: ${time[0]} hours; ${time[1]} minutes; ${time[2]<10 ? '0'+time[2] : time[2]} seconds</p>
+    `;
+    //Count down timer.
+    setInterval( ()=>{
+        if (time[2]==0 && time[1]>0){
+            time[1]--;
+            time[2] = 60;
+        }
+
+        if(time[2]==0 && time[1]==0 && time[0]>0){
+            time[0]--;
+            time[1] = 59;
+            time[2] = 60;
+        }
+
+        if (time[0]==0){
+            endgame.style.display = "none";
+        }
+        else {
+            time[2]--;
+            endgame.innerHTML = `
+                <p style="font-size: 2em">${correct}</p>
+                <p style="font-size: 1.5em">${(correct == "Correct")? "Keep it up tomorrow" : (correct == "Incorrect") ? "Try again tomorrow": ""}</p>
+                ${correct == "Correct" ? `<p style='font-size: 1.5em'>Your streak is ${streak}</p>` : (correct == "Incorrect") ? `<p style='font-size: 1.5em'>The correct answer is ${correctAnswer}</p>` : (streak>0) ? `<p style='font-size: 1.5em'>Your streak is ${streak}</p>` :""}
+                <p style="font-size: 1.5em">Your percentage is ${Math.round(score/total*100)}%</p>
+                <p style="font-size: 1.5em">Time until next puzzle: ${time[0]} hours; ${time[1]} minutes; ${time[2]<10 ? '0'+time[2] : time[2]} seconds</p>
+            `;
+        }
+        
+    },1000);
+}
+
+
+export function timeUntilEndOfDay(){
+    const d = new Date();
+    // console.log(d);
+    const h = d.getHours();
+    const m = d.getMinutes();
+    const s = d.getSeconds(); 
+    const time = [23-h, 59-m, 59-s];
+    return time;
+}
+
+export function todaysDate(){
+    const date = new Date();
+    const y = date.getFullYear();
+    const m = date.getMonth();
+    const d = date.getDate();
+
+    return `${y}-${m}-${d}`;
+}
+
+export function daysBetweenTwoDates (day1, day2){
+    const date1 = new Date(day1);
+    const date2 = new Date(day2);
+
+    const time1 = date1.getTime();
+    const time2 = date2.getTime();
+
+    const days = ((time2 - time1) / (1000 * 60 * 60 * 24));
+    return days;
+}
+
---
+++ script.js
@@ -0,0 +1,76 @@
+import { questionText, mcOptionsText, optionsClick, displayEndGame, todaysDate, timeUntilEndOfDay, daysBetweenTwoDates } from "./functions/question-text.js";
+
+//get Question Data
+getData()
+async function getData(){
+    fetch('data/data.json')
+    .then(response => response.json())
+    .then((data) => {
+        myApp(data);
+    });
+}
+
+//application function
+function myApp(data){
+    
+    let index = daysBetweenTwoDates("2024-11-3", todaysDate())%4;
+    
+    //get player data from local storage: current score, if they've already played todays game etc.
+    let playerData;
+    
+    if (localStorage.getItem("playerData") === null){
+        playerData = {
+            "score": 0,
+            "total": 0,
+            "streak": 0
+        };
+    }
+
+    else {
+        playerData = JSON.parse(localStorage.getItem("playerData"));
+    }
+
+    // console.log(playerData);
+
+    //Check if already played today => Comment out this block if you need.
+    if (Object.keys(playerData).includes('last-game-finished-date')){
+        if (playerData['last-game-finished-date'] == todaysDate()){
+            const endgame = document.querySelector(".end-game");
+            let time = timeUntilEndOfDay();
+            displayEndGame(endgame, playerData['score'], time, "rgba(132, 111, 252, 0.9)", "Play another puzzle tomorrow", "C", playerData["streak"], playerData['total'] );
+        }
+    }
+    
+    let app = document.querySelector(".wrapper"); //get the app of the html
+    let questionString = "";
+    
+    // for the question stem text
+    for (let n=0; n<data[index]['question']['stem'].length; n++){ //loop through the question structure to add it to the app
+        questionString = questionText(data[index]['question']['stem'][n], questionString); //function questionText peices together text from the data so it's displayed as, text, equations, images, italics etc.
+    }
+
+    // for the question text
+    for (let n=0; n<data[index]['question']['question-text'].length; n++){ //loop through the question structure to add it to the app
+        questionString = questionText(data[index]['question']['question-text'][n], questionString); //function questionText peices together text from the data so it's displayed as, text, equations, images, italics etc.
+    }
+
+    // MC options text
+    questionString = mcOptionsText(data[index]['question']['options'], questionString);
+    
+    app.innerHTML += questionString; //Add the questionString to the app of the HTML
+    MathJax.typesetPromise(); //Type set MathJax equations
+
+    //event listener for click in options-container
+    const optionsContainer = document.querySelector(".options-container");
+    optionsContainer.correctAnswer = data[index]['question']['correct-answer']["choice"];
+    optionsContainer.playerData = playerData;
+    optionsContainer.addEventListener("click", optionsClick);
+
+
+
+
+
+
+
+
+}
